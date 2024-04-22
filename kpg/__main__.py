9d8c3537cfe04e44aefaa9f253f10164238ed13f
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2024-04-21 23:40:56 -0400

    ribbing

--- kpg/__main__.py
+++ kpg/__main__.py
@@ -1,6 +1,7 @@
 from kpg.cli import read_cli_args
 from kpg.garter import garter
 from kpg.meeting_corners_square import meeting_corners_square
+from kpg.ribbing import ribbing
 from kpg.seed import seed
 from kpg.stockinette import stockinette
 
@@ -13,7 +14,7 @@ def main():
   elif args.pattern == "meeting_corners_square":
     meeting_corners_square(args.cast_on_count, args.expand)
   elif args.pattern == "ribbing":
-    print("Ribbing function will go here")
+    ribbing(args.cast_on_count, args.rows, args.expand)
   if args.pattern == "seed":
     seed(args.cast_on_count, args.rows, args.expand)
   elif args.pattern == "stockinette":
--- kpg/ribbing.py
+++ kpg/ribbing.py
@@ -1,19 +1,19 @@
-def garter(cast_on_count: int, rows: int, expand: bool) -> None:
+def ribbing(cast_on_count: int, rows: int, expand: bool) -> None:
   if cast_on_count < 10 or cast_on_count > 500:
     print("Cast-on count must be a number between 10 and 500. Try again!")
   elif rows < 10 or rows > 500:
     print("Row count must be between 10 and 500 for this pattern. Try again!")
   else:
-    print("Garter Knitting Pattern")
-    print("Makes a horizontal textured pattern. Worked in all knit stitches.")
+    print("Ribbing Knitting Pattern")
+    print("Makes a vertical textured pattern.")
     print(f"Cast on {cast_on_count} stitches.")
 
     if expand:
       for i in range(1, rows):
-        print(f"Row {i}: Knit.")
+        print(f"Row {i}: K1, P1 until reaching end of row.")
     else:
-      print("Row 1 (and all subsequent rows): Knit.")
+      print("Row 1 (and all subsequent rows): K1, P1 until reaching end of row.")
       print(f"Continue same pattern until reaching row {rows}.")
 
-    print(f"Row {rows}: Knit.")
+    print(f"Row {rows}: K1, P1 until reaching end of row.")
     print("Fasten off.")
